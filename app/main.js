4ed84bc71452f51bf8e9b1a1bd9453def7fea465
honger <honger05@126.com>
2015-09-23 18:04:26 +0800

    overlay

--- app/main.js
+++ app/main.js
@@ -1,35 +1,23 @@
 'use strict';
 
-var position = require('./component.js');
+var Overlay = require('./overlay.js');
 
-// 将 a1 定位到 b1 的  x: '50px',y: '50px' 位置
-position.pin('#a1', {
-  element: '#b1',
-  x: '50px',
-  y: '50px'
-})
-
-// 定位fixed元素, 无 element 表示可视区域
-position.pin('#a2', {
-  x: 400,
-  y: 0
-})
-
-// 处理类似于100%+20px这样的定位
-position.pin('#a3', {
-  element: '#b3',
-  x: '100%+50px',
-  y: '50%-50'
+var o = new Overlay({
+    template: "<div class='overlay'>目标元素1</div>",
+    parentNode: '#c',
+    id: 'myoverlay',
+    style: {
+        color: '#fff'
+    },
+    align: {
+        selfXY: ['-100%', 0],
+        baseElement: '#a',
+        baseXY: [0, 0]
+    }
 });
 
-// 定位到元素中央
-position.center('#a4', '#b4');
-
-// 目标元素带偏移量
-position.pin({
-  element: '#a5',
-  x: 10,
-  y: 30
-}, {
-  element: '#b5'
+o.show();
+o.set('style', {
+    backgroundColor: '#f53379'
 });
+o.set('height', 40);
